0ae2a918faa534e82aafc479b7a09885d092ee8a
Laurie Bezacier <laury136@hotmail.fr>
2022-02-02 18:32:39 +0100

    Boucle ForEach - suite

--- front/js/script.js
+++ front/js/script.js
@@ -1,8 +1,5 @@
-// Variable dont la valeur est le contenu de l'élément Id items
-let recupItems = document.getElementById(items);
-
 // Création des cartes
-// Fonction qui récupère les données des articles du catalogue de l'API
+// Fonction qui récupère les données des articles du catalogue de l'API + modification des informations des produits
 function recupDataAPI() {
     const response = fetch("http://localhost:3000/api/products")
         .then(function (response) {
@@ -10,27 +7,38 @@ function recupDataAPI() {
             return response.json()
         })
         .then(function (data) {
+            // Afficher les données sous forme de tableau
             console.table(data)
             // Création de la boucle qui récupère toutes les infos des produits
             for (let canap of data) {
+                console.log(canap.name)
                 // Récupérer le nom du produit
                 let nom = document.getElementsByClassName("productName");
-                console.log(canap.name)
                 // Modifier le nom du produit
-                nom.innerHTML = canap.name;
-                //console.log(nom)
-                // Récupérer l'image
-                let img = document.querySelectorAll('#items a.article > img');
+                //card 1 = nouveau nom de la card 1
+                nom[0].innerHTML = canap.name
+                nom[1].innerHTML = canap.name
+                nom[2].innerHTML = canap.name
+                nom[3].innerHTML = canap.name
+                nom[4].innerHTML = canap.name
+                nom[5].innerHTML = canap.name
+                nom[6].innerHTML = canap.name
+                nom[7].innerHTML = canap.name
+                console.log(nom[1])
+                
+                /* // Récupérer l'image
+                let img = document.querySelectorAll("#items a.article > img");
                 console.log(canap.imageUrl)
                 // Modifier l'image 
-                img.innerHTML = canap.imageUrl;
-                // Récupérer l'attribut alt
-                /* let alt = document.getElementById('items');
+                //img[0].innerHTML = canap.imageUrl
+                //console.log(img) */
+                /* // Récupérer l'attribut alt
+                 let alt = document.getElementById('items');
                 console.log(canap.altTxt)
                 // Modifier l'attribut alt
                 alt.innerHTML = canap.altTxt; */
-                // Récupérer la description
-                let descr = document.getElementsByClassName('productDescription');
+                /* // Récupérer la description
+                let descr = document.getElementsByClassName("productDescription");
                 console.log(canap.description)
                 // Modifier la description
                 descr.innerHTML = canap.description;
