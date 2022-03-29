6cd059022030ef218aa21db0420625b066aefcbc
Laurie Bezacier <laury136@hotmail.fr>
2022-03-29 19:17:24 +0200

    Modif. qte + Sup. canap (sur cart.js) (en cours)

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,6 +6,8 @@ let panierLS = localStorage.getItem("panier");
 
 if (panierLS !== null) {
     afficherProduits();
+} else {
+    console.log("Aucun produit n'est encore présent dans le panier")
 }
 
 function afficherProduits() {
@@ -68,33 +70,62 @@ function afficherProduits() {
                 // Couleur du produit sélectionné
                 clone.querySelector(".cart__item__content .couleurProduitSelect").innerText = canap.couleur;
 
-                // Quantité du produit sélectionné
-                clone.querySelector(".cart__item__content .cart__item__content__settings__quantity > input").setAttribute("value", canap.qte);
+                // Quantité (initiale) du produit sélectionné
+                let inputQte = clone.querySelector(".cart__item__content .cart__item__content__settings__quantity > input");
+                inputQte.setAttribute("value", canap.qte);
+
+
+                // Pour connaitre l'index de l'objet 'produit' (dont on souhaite modifier la quantité) dans le array 'panier JS'
+                const indexProduit = (item) => (item.idProduit == produit.idProduit) && (item.couleur == produit.couleur); // méthode 'item' : true (si 'idProduit' du 'panier(LS)' est identique à celui de l'objet JS 'produit' (de panier(JS))) ou false (si différent)
+                let index = panierLS.findIndex(indexProduit); // méthode 'findIndex' : '-1' s'il n'existe pas , sinon renvoie son 'index'
+
+                if (Input.addEventListener('change', changeQte(canap))) // Si qté modifiée
+                {
+                    // alors modification du panier dans LS
+                    let qteChange = parseInt(panierLS[index].qte) + parseInt(canap.qte);
+
+                    // Modification (de la valeur) de la quantité initiale
+                    panierLS[index].qte = qteChange;
+
+                    // Conversion du array (objet JS) en 'string' (pour pouvoir le re-stocker dans LS) 
+                    panierLS = JSON.stringify(panierJS); // string
+                    // Création d'une nouvelle valeur à la clé 'panier'
+                    localStorage.setItem("panier", panierLS);
+                    console.log("La quantité d'un produit a été modifiée (dans LS)")
+                }
+
+                function changeQte(canap) {
+                    qteInput.setAttribute("value", canap.qte);
+
+                    //canap.qte = qteInput.value;
+                };
+
+
 
                 // Ajout de nouveaux enfants (cards clonnées) à la fin de la liste des enfants (déjà existants) du parent 'cardParent'
                 cardParent.appendChild(clone);
+
+
+                // Supression d'un 'canap' dans le panier
+
+                let btnSup = document.querySelector(".cart__item__content__settings__delete .deleteItem")
+
+                btnSup.addEventListener('click', supCanap(produit));
+                console.log('btnSup fonctionne')
+
+                function supCanap(produit) {
+
+                    // Pour connaitre l'index de l'objet 'produit' (à supprimer) dans le array 'panier'
+                    const indexProduit = (item) => (item.idProduit == produit.idProduit) && (item.couleur == produit.couleur); // méthode 'item' : true (si 'idProduit' du 'panier(LS)' est identique à celui de l'objet JS 'produit' (de panier(JS))) ou false (si différent)
+                    let index = panierJS.findIndex(indexProduit); // méthode 'findIndex' : '-1' s'il n'existe pas , sinon renvoie son 'index'
+                    console.log(index)
+
+                    if (index !== -1) {
+                        panierJS.splice(index, 1);
+                        alert("le canap a bien été supprimé")
+                    }
+                };
             })
             .catch(function (err) {});
     };
-
-    // Supression d'un produit
-
-    let btnSup = document.querySelector(".cart__item__content__settings__delete > p")
-
-    btnSup.addEventListener("click", supCanap, false);
-    btnSup.removeEventListener("click", supCanap, true);
-
-    function supCanap() {
-        alert("le canap a bien été supprimé")
-
-        /* // Pour connaitre l'index de l'objet 'produit' (à supprimer) dans le array 'panier'
-        const indexProduit = (item) => (item.idProduit == produit.idProduit) && (item.couleur == produit.couleur); // méthode 'item' : true (si 'idProduit' du 'panier(LS)' est identique à celui de l'objet JS 'produit' (de panier(JS))) ou false (si différent)
-
-        let index = panierJS.findIndex(indexProduit); // méthode 'findIndex' : '-1' s'il n'existe pas , sinon renvoie son 'index'
-
-        if (index > -1) {
-            panierJS.splice(index, 1);
-            alert("le canap a bien été supprimé")
-        } */
-    };    
 };
